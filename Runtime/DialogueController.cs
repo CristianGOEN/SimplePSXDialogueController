e3088009176468a930759d66dfba801563f3a781
CristianGOEN <cristian.goen@gmail.com>
2026-02-22 19:52:01 +0100

    fix typo

--- Runtime/DialogueController.cs
+++ Runtime/DialogueController.cs
@@ -66,7 +66,7 @@ namespace SimplePSXDialogueController
 
         public void StartRandomDialogue(Dialogues dialogues)
         {
-            if (!dialogue)
+            if (!dialogues)
             {
                 return;
             }
